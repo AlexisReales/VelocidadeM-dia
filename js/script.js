735980170b7e9d20c9b24236e31bb1c08a0d1ec5
Alexis Reales <100932784+AlexisReales@users.noreply.github.com>
2022-09-22 13:05:34 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let distancia = document.querySelector("#distancia")
+let tempo = document.querySelector("#tempo")
+let velocidade = document.querySelector("#velocidade")
+let resultado = document.querySelector("#resultado")
+let arrayVelocidade = []
+
+const calcular = function() {
+    velocidade.value = (distancia.value / tempo.value)
+    arrayVelocidade.push([distancia.value," ", velocidade.value," ", tempo.value, " "])
+    resultado.value = arrayVelocidade
+}
